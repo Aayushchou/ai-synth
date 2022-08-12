a906ddbe15b3caa5425b6169f497077b8cc3d7d3
Aayushchou <aayushchou@gmail.com>
2022-08-12 15:56:51 +0100

    tested resnet block

--- src/aisynth/model/blocks.py
+++ src/aisynth/model/blocks.py
@@ -1,32 +1,42 @@
 import torch.nn as nn
+import torch
 import math
 
 
 class ConvBlock1D(nn.Module):
-    """Convolutional block with a residual architecture in 1D. Used in the encoder and decoders.
-    :param
-        n_in -- number of input channels
-        n_hidden -- number of hidden channels
-        stride -- stride for the first convolutional layer
-        padding -- padding for the first convolutional layer
-        dilation -- dilation for the first convolutional layer
-        scale -- scaling the model outputs to provide control over input/latent representation
-        zero_out -- whether to output zeros
-        dropout_flag -- including dropout in the block for regularisation
-        dropout_val -- how much drop out to add
-        batchnorm_flag -- whether to add batch normalisation
-    : returns
-        nn.Module object"""
+    """
+    Convolutional block with a residual architecture in 1D. Used in the encoder and decoders.
+
+            params:
+                n_in: number of input channels
+                n_hidden: number of hidden channels
+                stride: stride for the first convolutional layer
+                padding: padding for the first convolutional layer
+                dilation: dilation for the first convolutional layer
+                scale: scaling the model outputs to provide control over input/latent representation
+                zero_out: whether to output zeros
+                dropout_flag: including dropout in the block for regularisation
+                dropout_val: how much drop out to add
+                batchnorm_flag: whether to add batch normalisation
+
+            attr:
+                scale: Scaling factor of the residual block outputs
+                dropout_flag: Whether to add dropout
+                batchnorm_flag: Whether to add batch normalisation
+                layers: List containing layers in model
+                block: Module object with user defined layers
+
+    """
     def __init__(self,
-                 n_in,
-                 n_hidden,
-                 stride=1,
-                 dilation=1,
-                 scale=1.0,
-                 zero_out=False,
-                 dropout_flag=False,
-                 dropout_val=0.3,
-                 batchnorm_flag=False):
+                 n_in: int,
+                 n_hidden: int,
+                 stride: int = 1,
+                 dilation: int = 1,
+                 scale: float = 1.0,
+                 zero_out: bool = False,
+                 dropout_flag: bool = False,
+                 dropout_val: float = 0.3,
+                 batchnorm_flag: bool = False):
         """Setting up convolutional layers and normalisation based on parameters. """
         super().__init__()
         self.scale = scale
@@ -36,9 +46,9 @@ class ConvBlock1D(nn.Module):
         padding = dilation
         self.layers = [
             nn.ReLU(),
-            nn.Conv1d(n_in, n_hidden, 3, stride=stride, padding=padding, dilation=dilation),
+            nn.Conv1d(n_in, n_hidden, (3,), stride=(stride,), padding=padding, dilation=(dilation,)),
             nn.ReLU(),
-            nn.Conv1d(n_hidden, n_in, 1, stride=1, padding=0)
+            nn.Conv1d(n_hidden, n_in, (1,), stride=(1,), padding=0)
         ]
         if batchnorm_flag:
             self.layers.insert(0, nn.BatchNorm1d(n_in))
@@ -53,44 +63,66 @@ class ConvBlock1D(nn.Module):
             nn.init.zeros_(out.weight)
             nn.init.zeros_(out.bias)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """ Residual forward pass, input vector plus scaled hidden vector"""
         return x + self.scale * self.block(x)
 
 
 class ResidualBlock1D(nn.Module):
+    """
+    Residual block with dilated convolutions. Used in the encoder and decoders.
 
+            params:
+                n_in: number of input channels
+                n_depth: depth of the resnet
+                dilation_growth_rate: factor by which the dilation increases or decreases
+                dilation_cycle: if diilation should modulate around a value
+                zero_out: whether to output zeros
+                dropout_val: value of dropout for regularization
+                m_conv: scaling factor for hidden convolution dimension
+                scale: scaling factor on how to scale residual outputs
+                reverse_dilation: whether to reverse dilation pattern, used for decoder
+                batchnorm_flag: whether to add batch normalisation
+
+            attr:
+                blocks: list of convolutional blocks with specified parameters
+                resnet: torch sequential module for the resnet
+
+    """
     def __init__(self,
-                 n_in,
-                 n_depth,
-                 dilation_growth_rate=1,
-                 dilation_cycle=None,
-                 zero_out=False,
-                 dropout_val: int = 0,
-                 m_conv=1.0,
-                 scale=False,
-                 reverse_dilation=False):
+                 n_in: int,
+                 n_depth: int,
+                 dilation_growth_rate: int = 1,
+                 dilation_cycle: int = None,
+                 zero_out: bool = False,
+                 dropout_val: float = 0.0,
+                 m_conv: float = 1.0,
+                 scale: bool = False,
+                 reverse_dilation: bool = False):
+        """Defines list of residual convolutional blocks for the network. """
         super().__init__()
         self.dilation_cycle = dilation_cycle
         self.blocks = [ConvBlock1D(n_in=n_in,
                                    n_hidden=int(m_conv * n_in),
                                    dilation=dilation_growth_rate ** self._get_depth(depth),
                                    zero_out=zero_out,
-                                   dropout_flag=False if dropout_val == 0 else True,
+                                   dropout_flag=False if dropout_val == 0.0 else True,
                                    dropout_val=dropout_val,
                                    scale=1.0 if not scale else 1.0 / math.sqrt(n_depth))
-                       for depth in n_depth]
+                       for depth in range(n_depth)]
 
         if reverse_dilation:
             self.blocks = self.blocks[::-1]
 
         self.resnet = nn.Sequential(*self.blocks)
 
-    def _get_depth(self, depth):
+    def _get_depth(self, depth: int) -> int:
+        """returns depth based on the dilation cycle for calculating dilation"""
         return depth if self.dilation_cycle is None else depth % self.dilation_cycle
 
-    def forward(self, x):
-        # WATCH OUT FOR THIS addition, its what should be happening according to the diagram but isn't
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """forward pass through the model"""
+        # WATCH OUT FOR THIS ADDITION
         return x + self.resnet(x)
 
 
--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -1,12 +1,12 @@
 import pytest
 from aisynth.globals import TEST_INPUTS
-from aisynth.model.blocks import ConvBlock1D, EncoderBlock1D, DecoderBlock1D
+from aisynth.model.blocks import ConvBlock1D, ResidualBlock1D, EncoderBlock1D, DecoderBlock1D
 
 
 class TestConvBlock:
-    """Test suite for checking validity of the residual block object."""
+    """Test suite for checking validity of the convolutional block object."""
 
-    @pytest.mark.parametrize("inputs", TEST_INPUTS["1DRESBLOCK_TEST"])
+    @pytest.mark.parametrize("inputs", TEST_INPUTS["CONVBLOCK_TEST"])
     def test_init(self, inputs):
         """Tests initialisation and whether the metadata is generated appropriately"""
         rb = ConvBlock1D(n_in=inputs["n_in"],
@@ -15,13 +15,40 @@ class TestConvBlock:
                          dropout_flag=inputs["dropout_flag"])
         assert len(rb.layers) in [4, 6]
 
-    @pytest.mark.parametrize("inputs", TEST_INPUTS["1DRESBLOCK_TEST"])
+    @pytest.mark.parametrize("inputs", TEST_INPUTS["CONVBLOCK_TEST"])
     def test_forward(self, inputs):
         """Tests length of the dataset and whether it matches what we expect"""
         rb = ConvBlock1D(n_in=inputs["n_in"],
                          n_hidden=inputs["n_hidden"],
                          batchnorm_flag=inputs["batchnorm_flag"],
                          dropout_flag=inputs["dropout_flag"])
+
+        out = rb.forward(inputs["test_tensor"])
+        assert out.shape == inputs["test_tensor"].shape
+
+
+class TestResidualBlock:
+    """Test suite for checking validity of the residual block object."""
+
+    @pytest.mark.parametrize("inputs", TEST_INPUTS["RESBLOCK_TEST"])
+    def test_init(self, inputs):
+        """Tests initialisation and whether the metadata is generated appropriately"""
+        rb = ResidualBlock1D(n_in=inputs["n_in"],
+                             n_depth=inputs["n_depth"],
+                             dilation_cycle=inputs["dilation_cycle"],
+                             dilation_growth_rate=inputs["dilation_growth_rate"],
+                             reverse_dilation=inputs["reverse_dilation"])
+        assert len(rb.blocks) == inputs["n_depth"]
+
+    @pytest.mark.parametrize("inputs", TEST_INPUTS["RESBLOCK_TEST"])
+    def test_forward(self, inputs):
+        """Tests length of the dataset and whether it matches what we expect"""
+        rb = ResidualBlock1D(n_in=inputs["n_in"],
+                             n_depth=inputs["n_depth"],
+                             dilation_cycle=inputs["dilation_cycle"],
+                             dilation_growth_rate=inputs["dilation_growth_rate"],
+                             reverse_dilation=inputs["reverse_dilation"],
+                             m_conv=inputs["m_conv"])
 
         out = rb.forward(inputs["test_tensor"])
         assert out.shape == inputs["test_tensor"].shape

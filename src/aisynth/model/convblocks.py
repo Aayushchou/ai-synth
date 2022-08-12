ded9bc587bd7bb6bca7f7b422442e567bee1eab3
Aayushchou <aayushchou@gmail.com>
2022-08-12 02:24:18 +0100

    added 1d residual block

--- src/aisynth/model/convblocks.py
+++ src/aisynth/model/convblocks.py
@@ -1,13 +1,46 @@
 import torch.nn as nn
+import torch
 
 
-class ResidualBlock(nn.Module):
+class ResidualBlock1D(nn.Module):
 
-    def __init__(self):
+    def __init__(self,
+                 n_in,
+                 n_hidden,
+                 stride=1,
+                 dilation=1,
+                 scale=1.0,
+                 zero_out=False,
+                 dropout_flag=False,
+                 dropout_val=0.3,
+                 batchnorm_flag=False):
         super().__init__()
+        self.scale = scale
+        self.dropout_flag = dropout_flag
+        self.batchnorm_flag = batchnorm_flag
 
-    def forward(self):
-        pass
+        padding = dilation
+        layers = [
+            nn.ReLU(),
+            nn.Conv1d(n_in, n_hidden, 3, stride=stride, padding=padding, dilation=dilation),
+            nn.ReLU(),
+            nn.Conv1d(n_hidden, n_in, 1, stride=1, padding=0)
+        ]
+        if batchnorm_flag:
+            layers.insert(0, nn.BatchNorm1d(n_in))
+
+        if dropout_flag:
+            layers.insert(-1, nn.Dropout(dropout_val))
+
+        self.block = nn.Sequential(*layers)
+
+        if zero_out:
+            out = self.block[-1]
+            nn.init.zeros_(out.weight)
+            nn.init.zeros_(out.bias)
+
+    def forward(self, x):
+        return x + self.scale * self.block(x)
 
 
 class EncoderBlock(nn.Module):
@@ -26,3 +59,4 @@ class DecoderBlock(nn.Module):
 
     def forward(self):
         pass
+

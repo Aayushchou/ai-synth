6a0fecbd0eb2487abb677017333d288f566607fd
Aayushchou <aayushchou@gmail.com>
2022-08-12 20:48:47 +0100

    prepped train files

--- src/aisynth/globals.py
+++ src/aisynth/globals.py
@@ -1,16 +1,16 @@
-import torch
+from torch import rand
 
 TEST_INPUTS = {"AUDIO_DIR_TEST": [{"audio_dir": r"test_resources/dx7"}],
                "CONVBLOCK_TEST": [{"n_in": 10,
                                    "n_hidden": 10,
                                    "batchnorm_flag": False,
                                    "dropout_flag": False,
-                                   "test_tensor": torch.rand(10, 10)},
+                                   "test_tensor": rand(10, 10)},
                                   {"n_in": 10,
                                    "n_hidden": 10,
                                    "batchnorm_flag": True,
                                    "dropout_flag": True,
-                                   "test_tensor": torch.rand(10, 10)}
+                                   "test_tensor": rand(10, 10)}
                                   ],
                "RESBLOCK_TEST": [{"n_in": 10,
                                   "n_depth": 2,
@@ -18,14 +18,14 @@ TEST_INPUTS = {"AUDIO_DIR_TEST": [{"audio_dir": r"test_resources/dx7"}],
                                   "dilation_cycle": None,
                                   "reverse_dilation": False,
                                   "m_conv": 1,
-                                  "test_tensor": torch.rand(10, 10)},
+                                  "test_tensor": rand(10, 10)},
                                  {"n_in": 10,
                                   "n_depth": 6,
                                   "dilation_growth_rate": 4,
                                   "dilation_cycle": None,
                                   "reverse_dilation": True,
                                   "m_conv": 2,
-                                  "test_tensor": torch.rand(10, 10)}
+                                  "test_tensor": rand(10, 10)}
                                  ]
 
                }
